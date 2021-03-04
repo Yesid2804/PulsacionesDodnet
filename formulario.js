f466830591a989253eeab961050840b26c6422b7
Yesid Hernandez <yesher2804@gmail.com>
2021-03-04 18:20:18 -0500

    Registro en localStorage

--- formulario.js
+++ formulario.js
@@ -1,26 +1,45 @@
-function calcularPulsaciones(){
+function getGenero(){
     var radios = document.getElementsByName('genero');
-    var inputEdad = document.getElementsByName('edad');
-
-    let genero = "";
-    var pulsaciones = 0;
-
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-            genero = radios[i].value
-            break;
+            return radios[i].value
         }
     }
-
-    if (genero=="mujer") {
-        let idEdad = document.getElementById("edad").value;
-        pulsaciones = (220-idEdad)/10;
-    }
-    if (genero=="hombre") {
-        let idEdad = document.getElementById("edad").value;
-        pulsaciones = (210-idEdad)/10;
-    }
-     document.getElementById("idResultado").value = "Pulsaciones: " +  pulsaciones; 
 }
 
+function calcularPulsaciones(genero,edad){
+    var pulsaciones = 0;
 
+    if (genero=="mujer") {
+        let edad = document.getElementById("edad").value;
+        pulsaciones = (220-edad)/10;
+    }
+    if (genero=="hombre") {
+        let edad = document.getElementById("edad").value;
+        pulsaciones = (210-edad)/10;
+    }
+    return pulsaciones;
+}
+
+function registrar(){
+    var nombre = document.getElementById('nombre').value;
+    var edad = document.getElementById('edad').value;
+    let genero = getGenero();
+    let pulsaciones = calcularPulsaciones(genero,edad);
+    var personas = localStorage.getItem('personas');
+    if(personas == undefined)
+        personas = []
+    else
+        personas = JSON.parse(personas)
+        
+        var nuevaPersona = {
+            nombre,
+            edad,
+            genero,
+            pulsaciones
+    }
+    
+    personas.push(nuevaPersona);
+    localStorage.setItem('personas',JSON.stringify(personas));
+    document.getElementById("idResultado").value = "Pulsaciones: " +  pulsaciones; 
+}
